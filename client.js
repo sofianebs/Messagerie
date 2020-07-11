ffd4f0bbbecbd81c24889922c8aaea88f5ff164f
sofiane bs <benslimenesofien@gmail.com>
2020-07-11 20:15:43 +0200

    Avancement

--- client.js
+++ client.js
@@ -1,19 +1,22 @@
 $(function () {
+
+
     let socket = io();
     let ID = Date.now();
     socket.emit('login', { id: ID })
     $('#action_menu_btn').click(function () {
         $('.action_menu').toggle();
     });
-
+    // Prompt pour selection fichier image
     $('#btn').on('click', function () {
         $('#open').trigger('click');
+
     });
+    // append image pour user emmeteur
     function imageIsLoaded(e) {
         var x = 'foo';
         var picture = '<img src="' + e.target.result + '" style="width:200px;height:200px;" class="' + x + 'thImage">'
-
-        $('#messages').append('<div class="d-flex justify-content-end mb-4"> <div class="msg_cotainer_send"> ' + picture + ' </div><div class="img_cont_msg"> <img src="https://api.adorable.io/avatars/167/" class="rounded-circle user_img_msg"></div></div>');
+        $('#messages').append('<div class="d-flex justify-content-end mb-4"> <div class="msg_cotainer_send"> ' + picture + ' </div><div class="img_cont_msg"> <img src="https://api.adorable.io/avatars/167/' + ID + '" class="rounded-circle user_img_msg"></div></div>');
 
     }
 
@@ -21,21 +24,26 @@ $(function () {
     $("#input-message").on("input", function () {
         socket.emit('Iswriting', { id: ID });
     });
-    $("#open").on("change", function () {
+
+
+    $("#open").on("change", function (e) {
         if (this.files && this.files[0]) {
             var reader = new FileReader();
             reader.onload = imageIsLoaded;
             reader.readAsDataURL(this.files[0]);
-           
-
+            var file = this.files[0] ;
+            var formData = new FormData();
+            formData.append('file',file);
+            formData.append('id',ID) ;
+            
             $.ajax({
                 url: '/upload',
+                data: formData,
                 type: 'POST',
-                data: this.files ,
-                processData: false,  // tell jQuery not to process the data
-                contentType: false,  // tell jQuery not to set contentType
-                success: function (data) {
-                  alert() ;
+                contentType: false,
+                processData: false, 
+                success: function(data){
+                    console.log(data);
                 }
             });
         }
@@ -46,30 +54,36 @@ $(function () {
     $('#send').click(function (e) {
         e.preventDefault();
         let text = $('#input-message').val();
-        socket.emit('chat message', text);
+        socket.emit('chat message', text, ID);
         $('#input-message').val('');
-        $('#messages').append('<div class="d-flex justify-content-end mb-4"> <div class="msg_cotainer_send"> ' + text + ' </div><div class="img_cont_msg"> <img src="https://api.adorable.io/avatars/167/" class="rounded-circle user_img_msg"></div></div>');
+        $('#messages').append('<div class="d-flex justify-content-end mb-4"> <div class="msg_cotainer_send"> ' + text + ' </div><div class="img_cont_msg"> <img src="https://api.adorable.io/avatars/167/' + ID + '" class="rounded-circle user_img_msg"></div></div>');
 
         return false;
     });
 
     socket.on('newusr', function (user) {
-        $('#membre').append('<li id="user-' + user + '"><div class="d-flex bd-highlight"> <div class="img_cont"> <img src="https://api.adorable.io/avatars/167/" class="rounded-circle user_img"> <span class="online_icon"></span></div><div class="user_info"><span>' + user + '</span><p id="stat' + user + '"></p></div></div> </li>');
+        $('#membre').append('<li id="user-' + user + '"><div class="d-flex bd-highlight"> <div class="img_cont"> <img src="https://api.adorable.io/avatars/167/' + user + '" class="rounded-circle user_img"> <span class="online_icon"></span></div><div class="user_info"><span>' + user + '</span><p id="stat' + user + '"></p></div></div> </li>');
     })
-
-
 
 
     socket.on('delusr', function (user) {
         $('#user-' + user).remove();
     })
 
-    socket.on('chat message', function (msg) {
-        $('#messages').append('<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://api.adorable.io/avatars/167/" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + msg + '<span class="msg_time">9:00 AM, Today</span></div></div>');
+    socket.on('chat message', function (msg, ID) {
+        $('#messages').append('<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://api.adorable.io/avatars/167/' + ID + '" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + msg + '<span class="msg_time">9:00 AM, Today</span></div></div>');
     })
     socket.on('Notification', function (msg) {
 
         $('#stat' + msg.id).append('entrain de ecrire un message ... ');
+
+    })
+    socket.on("MonProfile", function (ID) {
+        //console.log('im here'+ID) ;
+        $('#ME').append('<img src="https://api.adorable.io/avatars/100/' + ID + '" class="rounded-circle user_img"><span class="online_icon"></span>')
+    })
+    // Diffuser l'image au reste user 
+    socket.on("SharePic", function (Path){
 
     })
 });
--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const config = require('./config')
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const fs = require('fs'); 
-const path = require('path') 
-const fileUpload = require('express-fileupload');
-app.use(fileUpload({ safeFileNames: true, preserveExtension: true }))
+var multer = require('multer') 
+
+var upload = multer({dest : 'uploads/'}) ;
 app.get('/', function (req, res) {
     res.sendFile('/home/sofiane/Desktop/NodeRendu/CoursWebSockets/index.html')
 })
@@ -18,11 +17,11 @@ app.get('/client.js', function (req, res) {
     res.sendFile(__dirname + '/client.js')
 })
 
-app.post('/upload', function(req, res) {
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    console.log(req);
-
-});
+// GET Photo from AJax Post and diffuse au reste USers
+app.post('/upload', upload.single('file'), (req, res, next) => {
+    const file = req.file ;
+    return res.status(200).send({'path':file.path,'id':req.id});
+  })
 let users = {}
 
 io.on('connection', (socket) => {
@@ -35,10 +34,12 @@ io.on('connection', (socket) => {
     }
 
     socket.on('login', (user) => {
-       me = user.id;
-       console.log(me)
-       users[me] = me;
-       io.emit('newusr', me)
+        me = user.id;
+
+        users[me] = me;
+        io.emit('newusr', me)
+        socket.emit('MonProfile', me)
+
     })
 
     socket.on('disconnect', () => {
@@ -46,12 +47,12 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('delusr', me)
         console.log("diconnect")
     })
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message' , msg)
+    socket.on('chat message', (msg, ID) => {
+        socket.broadcast.emit('chat message', msg, ID)
     })
     socket.on('Iswriting', (user) => {
-        socket.broadcast.emit('Notification' , user)
-       
+        socket.broadcast.emit('Notification', user)
+
     })
 });
 
